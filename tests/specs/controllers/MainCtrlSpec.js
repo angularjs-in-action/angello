ecab618a190a123a1eda7639a269102631cfe603
simpulton <simpul@gmail.com>
2014-03-03 16:12:26 -0700

    Formatting

--- tests/specs/controllers/MainCtrlSpec.js
+++ tests/specs/controllers/MainCtrlSpec.js
@@ -2,30 +2,30 @@
 
 describe('Controller: MainCtrl', function () {
 
-  // load the controller's module
-  beforeEach(module('Angello'));
+    // load the controller's module
+    beforeEach(module('Angello'));
 
-  var MainCtrl,
-    scope;
+    var MainCtrl,
+        scope;
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller) {
-    scope = {};
-    MainCtrl = $controller('MainCtrl', {
-      $scope: scope,
-      angelloModel: angelloModelMock()
+    // Initialize the controller and a mock scope
+    beforeEach(inject(function ($controller) {
+        scope = {};
+        MainCtrl = $controller('MainCtrl', {
+            $scope: scope,
+            angelloModel: angelloModelMock()
+        });
+    }));
+
+    it('should attach a list of story types to the scope', function () {
+        expect(scope.types).toBeDefined();
     });
-  }));
 
-  it('should attach a list of story types to the scope', function () {
-    expect(scope.types).toBeDefined();
-  });
+    it('should attach a list of story statuses to the scope', function () {
+        expect(scope.statuses).toBeDefined();
+    });
 
-  it('should attach a list of story statuses to the scope', function () {
-    expect(scope.statuses).toBeDefined();
-  });
-
-  it('should attach a list of stories to the scope', function () {
-    expect(scope.stories).toBeDefined();
-  });
+    it('should attach a list of stories to the scope', function () {
+        expect(scope.stories).toBeDefined();
+    });
 });
