9e7891db837abaa23bc4f9ad336fef96def42ebe
simpulton <simpul@gmail.com>
2014-03-04 14:28:37 -0700

    Added in users and upgraded to Bootstrap 3
    TODO Regression in dashboard - stories is an an object and not an array

--- app/js/app.js
+++ app/js/app.js
@@ -3,6 +3,7 @@ var myModule = angular.module('Angello', ['ngRoute', 'ngAnimate']);
 myModule.config(function ($routeProvider) {
     $routeProvider.
         when('/', {templateUrl: 'partials/main.html', controller: 'MainCtrl'}).
+        when('/users', {templateUrl: 'partials/users.html', controller: 'UsersCtrl'}).
         when('/dashboard', {templateUrl: 'partials/dashboard.html', controller: 'DashboardCtrl'}).
         otherwise({redirectTo: '/'});
 });
@@ -78,6 +79,102 @@ myModule.factory('StoriesService', function ($http, $q, ENDPOINT_URI) {
         destroy: destroy
     };
 });
+
+myModule.controller('UsersCtrl', ['$scope', 'UsersService', function ($scope, UsersService) {
+    $scope.newUser = { name: '', email: '' };
+    $scope.users = {};
+
+    $scope.getUsers = function () {
+        UsersService.find().then(function (result) {
+            $scope.users = (result !== 'null') ? result : {};
+        }, function (reason) {
+            console.log('ERROR', reason);
+        });
+    };
+
+    $scope.addUser = function () {
+        UsersService.create(angular.copy($scope.newUser)).then(function (result) {
+            $scope.getUsers();
+            $scope.newUser = { name: '', email: '' };
+        }, function (reason) {
+            console.log('ERROR', reason);
+        });
+    };
+
+    $scope.updateUser = function (id, user) {
+        UsersService.update(id, user).then(function (result) {
+            // console.log('RESULT', result);
+        }, function (reason) {
+            console.log('ERROR', reason);
+        });
+    };
+
+    $scope.removeUser = function (id) {
+        UsersService.destroy(id).then(function (result) {
+            $scope.getUsers();
+        }, function (reason) {
+            console.log('ERROR', reason);
+        });
+    };
+
+    $scope.getUsers();
+}]);
+
+myModule.factory('UsersService', function ($http, $q, ENDPOINT_URI) {
+    var find = function () {
+        var deferred = $q.defer();
+        var url = ENDPOINT_URI + 'users.json';
+
+        $http.get(url).success(deferred.resolve).error(deferred.reject);
+
+        return deferred.promise;
+    };
+
+    var fetch = function (user_id) {
+        var deferred = $q.defer();
+        var url = ENDPOINT_URI + '/users/' + user_id + '.json';
+
+        $http.get(url).success(deferred.resolve).error(deferred.reject)
+
+        return deferred.promise;
+    };
+
+    var create = function (user) {
+        var deferred = $q.defer();
+        var url = ENDPOINT_URI + 'users.json';
+
+        $http.post(url, user).success(deferred.resolve).error(deferred.reject);
+
+        return deferred.promise;
+    };
+
+    var update = function (user_id, user) {
+        var deferred = $q.defer();
+        var url = ENDPOINT_URI + '/users/' + user_id + '.json';
+
+        $http.put(url, user).success(deferred.resolve).error(deferred.reject);
+
+        return deferred.promise;
+    };
+
+    var destroy = function (user_id) {
+        var deferred = $q.defer();
+        var url = ENDPOINT_URI + '/users/' + user_id + '.json';
+
+        $http.delete(url).success(deferred.resolve).error(deferred.reject);
+
+        return deferred.promise;
+    };
+
+    return {
+        find: find,
+        fetch: fetch,
+        create: create,
+        update: update,
+        destroy: destroy
+    };
+});
+
 
 myModule.factory('HelperService', function () {
     var buildIndex = function (source, property) {
