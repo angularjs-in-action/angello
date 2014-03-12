15fbd4d880b5feedb80ae5336f5c42d75b3989e9
Lukas Ruebbelke <simpul@gmail.com>
2014-03-11 22:56:17 -0700

    Fixing details animation

--- app/js/app.js
+++ app/js/app.js
@@ -549,8 +549,28 @@ myModule.directive('chart', function () {
     };
 });
 
+myModule.animation('.list-area-expanded', function () {
+    return {
+        addClass: function (element, className, done) {
+            if (className == 'list-area-expanded') {
+                TweenMax.to(element, 0.5, {right: 0, onComplete: done });
+            }
+            else {
+                done();
+            }
+        },
+        removeClass: function (element, className, done) {
+            if (className == 'list-area-expanded') {
+                TweenMax.to(element, 0.5, {right: 250, onComplete: done });
+            }
+            else {
+                done();
+            }
+        }
+    };
+});
 
-myModule.animation('.details-animation', function ($window) {
+myModule.animation('.details-animation', function () {
     return {
         addClass: function (element, className, done) {
             if (className == 'details-visible') {
