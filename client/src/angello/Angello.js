1806a1694789b37625e758729e0e9cbb9868fbaf
Lukas Ruebbelke <simpul@gmail.com>
2014-08-12 11:36:14 -0700

    Adding in $log decorator

--- client/src/angello/Angello.js
+++ client/src/angello/Angello.js
@@ -10,7 +10,7 @@ var myModule = angular.module('Angello',
         'Angello.User'
     ]);
 
-myModule.config(function ($routeProvider, $httpProvider) {
+myModule.config(function ($routeProvider, $httpProvider,$provide) {
     var getCurrentUser = function (AuthService, $location) {
         return AuthService.getCurrentUser().then(function (user) {
             if (!user) $location.path('/login');
@@ -64,7 +64,61 @@ myModule.config(function ($routeProvider, $httpProvider) {
         }).
         otherwise({redirectTo: '/'});
 
+    // Interceptor
     $httpProvider.interceptors.push('loadingInterceptor');
+
+
+    // Decorator
+    // Use the `decorator` solution to substitute or attach behaviors to
+    // original service instance; @see angular-mocks for more examples....
+    $provide.decorator( '$log', function( $delegate )
+    {
+        // Save the original $log.debug()
+        var debugFn = $delegate.debug;
+
+        function timeStamp() {
+            // Create a date object with the current time
+            var now = new Date();
+
+            // Create an array with the current month, day and time
+            var date = [ now.getMonth() + 1, now.getDate(), now.getFullYear() ];
+
+            // Create an array with the current hour, minute and second
+            var time = [ now.getHours(), now.getMinutes(), now.getSeconds() ];
+
+            // Determine AM or PM suffix based on the hour
+            var suffix = ( time[0] < 12 ) ? "AM" : "PM";
+
+            // Convert hour from military time
+            time[0] = ( time[0] < 12 ) ? time[0] : time[0] - 12;
+
+            // If hour is 0, set it to 12
+            time[0] = time[0] || 12;
+
+            // If seconds and minutes are less than 10, add a zero
+            for (var i = 1; i < 3; i++) {
+                if (time[i] < 10) {
+                    time[i] = "0" + time[i];
+                }
+            }
+
+            // Return the formatted string
+            return date.join("/") + " " + time.join(":") + " " + suffix;
+        }
+
+        $delegate.debug = function( )
+        {
+            var args    = [].slice.call(arguments);
+
+            // Prepend timestamp
+            args[0] = timeStamp() + ' - ' + args[0];
+
+            // Call the original with the output prepended with formatted timestamp
+            debugFn.apply(null, args)
+        };
+
+        return $delegate;
+    });
 });
 
 myModule.factory('loadingInterceptor', function(LoadingService) {
@@ -108,5 +162,3 @@ myModule.value('STORY_TYPES', [
 
 myModule.constant('ENDPOINT_URI', 'https://angello.firebaseio.com/');
 myModule.constant('Firebase', window.Firebase);
-
-
--- client/src/angello/storyboard/controllers/StoryboardController.js
+++ client/src/angello/storyboard/controllers/StoryboardController.js
@@ -1,95 +1,99 @@
 angular.module('Angello.Storyboard')
     .controller('StoryboardCtrl',
-        function ($scope, StoriesService, UsersService, STORY_STATUSES, STORY_TYPES) {
-            var myStory = this;
+    function ($scope, $log, StoriesService, UsersService, STORY_STATUSES, STORY_TYPES) {
+        var myStory = this;
 
-            myStory.detailsVisible = true;
-            myStory.currentStoryId = null;
-            myStory.currentStory = null;
-            myStory.editedStory = {};
-            myStory.stories = [];
+        myStory.detailsVisible = true;
+        myStory.currentStoryId = null;
+        myStory.currentStory = null;
+        myStory.editedStory = {};
+        myStory.stories = [];
 
-            myStory.types = STORY_TYPES;
-            myStory.statuses = STORY_STATUSES;
+        myStory.types = STORY_TYPES;
+        myStory.statuses = STORY_STATUSES;
 
-            myStory.users = {};
+        myStory.users = {};
 
-            UsersService.all()
-                .then(function (result) {
-                    myStory.users = (result.data !== 'null') ? result.data : {};
-                }, function (reason) {
-                    console.log('ERROR', reason);
-                });
-
-
-            myStory.setCurrentStory = function (id, story) {
-                console.log(id, story)
-                myStory.currentStoryId = id;
-                myStory.currentStory = story;
-                myStory.editedStory = angular.copy(myStory.currentStory);
-            };
-
-            myStory.getStories = function () {
-                StoriesService.all().then(function (result) {
-                    myStory.stories = (result.data !== 'null') ? result.data : {};
-                }, function (reason) {
-                    console.log('ERROR', reason);
-                });
-            };
-
-            myStory.createStory = function () {
-                StoriesService.create(myStory.editedStory).then(function (result) {
-                    myStory.getStories();
-                    myStory.resetForm();
-                }, function (reason) {
-                    console.log('ERROR', reason);
-                });
-            };
-
-            myStory.updateStory = function () {
-                var fields = ['title', 'description', 'criteria', 'status', 'type', 'reporter', 'assignee'];
-
-                fields.forEach(function (field) {
-                    myStory.currentStory[field] = myStory.editedStory[field]
-                });
-
-                StoriesService.update(myStory.currentStoryId, myStory.editedStory).then(function (result) {
-                    myStory.getStories();
-                    myStory.resetForm();
-                }, function (reason) {
-                    console.log('ERROR', reason);
-                });
-            };
-
-            myStory.updateCancel = function () {
-                myStory.resetForm();
-            };
-
-            myStory.resetForm = function () {
-                myStory.currentStory = null;
-                myStory.editedStory = {};
-
-                myStory.detailsForm.$setPristine();
-            };
-
-            myStory.setDetailsVisible = function (visible) {
-                myStory.detailsVisible = visible;
-            };
-
-            myStory.storiesWithStatus = function (status) {
-                var stories = {};
-                var keys = Object.keys(myStory.stories);
-                for (var i = 0, len = keys.length; i < len; i++) {
-                    var key = keys[i];
-                    if (myStory.stories[key].status == status.name) stories[key] = myStory.stories[key];
-                }
-                return stories;
-            };
-
-            $scope.$on('storyDeleted', function () {
-                myStory.getStories();
-                myStory.resetForm();
+        UsersService.all()
+            .then(function (result) {
+                myStory.users = (result.data !== 'null') ? result.data : {};
+                $log.debug('RESULT', result);
+            }, function (reason) {
+                $log.debug('REASON', reason);
             });
 
+        myStory.setCurrentStory = function (id, story) {
+            console.log(id, story)
+            myStory.currentStoryId = id;
+            myStory.currentStory = story;
+            myStory.editedStory = angular.copy(myStory.currentStory);
+        };
+
+        myStory.getStories = function () {
+            StoriesService.all().then(function (result) {
+                myStory.stories = (result.data !== 'null') ? result.data : {};
+                $log.debug('RESULT', result);
+            }, function (reason) {
+                $log.debug('REASON', reason);
+            });
+        };
+
+        myStory.createStory = function () {
+            StoriesService.create(myStory.editedStory)
+                .then(function (result) {
+                    myStory.getStories();
+                    myStory.resetForm();
+                }, function (reason) {
+                    console.log('ERROR', reason);
+                });
+        };
+
+        myStory.updateStory = function () {
+            var fields = ['title', 'description', 'criteria', 'status', 'type', 'reporter', 'assignee'];
+
+            fields.forEach(function (field) {
+                myStory.currentStory[field] = myStory.editedStory[field]
+            });
+
+            StoriesService.update(myStory.currentStoryId, myStory.editedStory)
+                .then(function (result) {
+                    myStory.getStories();
+                    myStory.resetForm();
+                    $log.debug('RESULT', result);
+                }, function (reason) {
+                    $log.debug('REASON', reason);
+                });
+        };
+
+        myStory.updateCancel = function () {
+            myStory.resetForm();
+        };
+
+        myStory.resetForm = function () {
+            myStory.currentStory = null;
+            myStory.editedStory = {};
+
+            myStory.detailsForm.$setPristine();
+        };
+
+        myStory.setDetailsVisible = function (visible) {
+            myStory.detailsVisible = visible;
+        };
+
+        myStory.storiesWithStatus = function (status) {
+            var stories = {};
+            var keys = Object.keys(myStory.stories);
+            for (var i = 0, len = keys.length; i < len; i++) {
+                var key = keys[i];
+                if (myStory.stories[key].status == status.name) stories[key] = myStory.stories[key];
+            }
+            return stories;
+        };
+
+        $scope.$on('storyDeleted', function () {
             myStory.getStories();
+            myStory.resetForm();
         });
+
+        myStory.getStories();
+    });
