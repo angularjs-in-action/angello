819554f1184724b18c5030699b57ae93274131b3
jdgarvey <jon.d.garvey@gmail.com>
2015-02-23 10:23:02 -0700

    Remove logout function from MainCtrl

--- client/src/angello/app/controllers/MainController.js
+++ client/src/angello/app/controllers/MainController.js
@@ -1,8 +1,2 @@
 angular.module('Angello.Common', [])
-    .controller('MainCtrl', function MainCtrl($location) {
-        var main = this;
-
-        main.logout = function() {
-            $location.path('login');
-        };
-    });
+    .controller('MainCtrl', function () {});
