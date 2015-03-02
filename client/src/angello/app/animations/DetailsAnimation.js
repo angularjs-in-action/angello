82905ff1e9e5329088a0ce13d620e67d82b46ad3
jdgarvey <jon.d.garvey@gmail.com>
2015-03-02 13:46:47 -0700

    Chapters 5 and 6 refactoring

--- client/src/angello/app/animations/DetailsAnimation.js
+++ client/src/angello/app/animations/DetailsAnimation.js
@@ -1,24 +1,25 @@
 angular.module('Angello.Common')
-    .animation('.details-animation', function () {
-        return {
-            addClass: function (element, className, done) {
-                if (className == 'details-visible') {
-                    TweenMax.to(element, 0.5, {right: 0, onComplete: done });
+    .animation('.details-animation',
+        function () {
+            return {
+                addClass: function (element, className, done) {
+                    if (className == 'details-visible') {
+                        TweenMax.to(element, 0.5, {right: 0, onComplete: done });
+                    }
+                    else {
+                        done();
+                    }
+                },
+                removeClass: function (element, className, done) {
+                    if (className == 'details-visible') {
+                        TweenMax.to(element, 0.5, {
+                            right: -element.width() + 50,
+                            onComplete: done
+                        });
+                    }
+                    else {
+                        done();
+                    }
                 }
-                else {
-                    done();
-                }
-            },
-            removeClass: function (element, className, done) {
-                if (className == 'details-visible') {
-                    TweenMax.to(element, 0.5, {
-                        right: -element.width() + 50,
-                        onComplete: done
-                    });
-                }
-                else {
-                    done();
-                }
-            }
-        };
-});
+            };
+        });
